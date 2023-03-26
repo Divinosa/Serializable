15abb0af1a241a6228437621dec936d266ee7ebc
“Danila” <Danilabirulevo@gmail.com>
2023-03-26 20:51:21 +0300

    json 1

--- src/Basket.java
+++ src/Basket.java
@@ -20,52 +20,16 @@ public class Basket {
     }
 
     public void saveTxt(File txtFile) throws IOException {
-        String[] myProducts = new String[cart.length];
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < cart.length; i++) {
-            sb.append(cart[i].getProductName()).append(" ");
-            sb.append(cart[i].getProductCount()).append(" ");
-            sb.append(cart[i].getProductPrice()).append(" ");
-            myProducts[i] = sb.toString();
-            sb.setLength(0);
-        }
-        OutputStream outputStream = new FileOutputStream(txtFile);
-        for (String pr : myProducts) {
-            outputStream.write(pr.getBytes(StandardCharsets.UTF_8));
-        }
+        FileOutputStream out = new FileOutputStream(txtFile);
+        ObjectOutputStream obj = new ObjectOutputStream(out);
+        obj.writeObject(this);
+        obj.close();
     }
 
-    static Product[] loadFromTxtFile(File textFile) throws IOException {
-        InputStream inputStream = new FileInputStream(textFile);
-        StringBuilder sb = new StringBuilder();
-        int a = 0;
-        while ((a = inputStream.read()) != -1) {
-            sb.append(Character.toString(a));
-        }
-        String[] save = sb.toString().split(" ");
-        Product[] products = new Product[save.length / 3];
-        for (int p = 0; p < products.length; p++) {
-            products[p] = new Product(null, 0, 0);
-        }
-        String[][] objSave = new String[products.length][3];
-        int p = 0;
-        for (int i = 0; i < objSave.length; i++) {
-            for (int c = 0; c < objSave.length; c++) {
-                objSave[i][c] = save[p];
-                p++;
-            }
-        }
-        for (int i = 0; i < objSave.length; i++) {
-            for (int c = 0; c < objSave.length; c++) {
-                if (c == 0) {
-                    products[i].setProductName(objSave[i][c]);
-                } else if (c == 1) {
-                    products[i].setProductCount(Integer.parseInt(objSave[i][c]));
-                } else {
-                    products[i].setProductCount(Integer.parseInt(objSave[i][c]));
-                }
-            }
-        }
-        return products;
+    static Product[] loadFromTxtFile(File textFile) throws IOException, ClassNotFoundException {
+        FileInputStream inputStream = new FileInputStream(textFile);
+        ObjectInputStream obf = new ObjectInputStream(inputStream);
+        Basket basket = (Basket) obf.readObject();
+        return basket.cart;
     }
 }
